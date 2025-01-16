9a63e838f21efe34574773bf9ae733de7bccb613
dmmosh <evilsharkeater@gmail.com>
2025-01-16 00:16:33 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -110,8 +110,8 @@ void loop() {
       connect_wait();
     }  
     digitalWrite(LED,ON);
-    int x = (2750-analogRead(VRX))/12;
-    int y = (2730-analogRead(VRY))/12;
+    int8_t x = (int8_t)((2750-analogRead(VRX))/12);
+    int8_t y = (int8_t)((2730-analogRead(VRY))/12);
 
     x /= (x>0) ? 2 : 1;
     y /= (y>0) ? 2 : 1;
