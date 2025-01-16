cb14ebabeee8cdbd2cff420a2143e338f0a6a442
dmmosh <evilsharkeater@gmail.com>
2025-01-15 21:24:25 -0500

    nothing of note

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,7 +13,7 @@ void gatts_event_handler(esp_gatts_cb_event_t event,
     switch (event) {
         case ESP_GATTS_CONGEST_EVT:
             if (param->congest.congested) {
-                Serial.println("CONGESTED")
+                Serial.println("CONGESTED");
             } else {
                 Serial.println("CLEARED");
             }
