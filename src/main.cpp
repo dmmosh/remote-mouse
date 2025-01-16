5b8ee31288f8f99c7702d381203db77c37a160fb
dmmosh <evilsharkeater@gmail.com>
2025-01-15 19:06:21 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,10 @@
 #define OFF 0
 #define ON 1
 
+// Report IDs:
+#define KEYBOARD_ID 0x01
+#define MEDIA_KEYS_ID 0x02
+#define MOUSE_ID 0x03
 
 // variables for the bluetooth server and hid device
 BLEHIDDevice* hid;
@@ -102,35 +106,36 @@ bool str_equals(const char* str1, const char* str2){
 
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting BLE work!");
-  pinMode(LED,OUTPUT);
-  BLEDevice::init("Remote Mouse");
-  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9); 
-  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
-  esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN ,ESP_PWR_LVL_P9);
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyCallbacks());
-  pServer->getPeerDevices(false);
+    Serial.begin(115200);
+    Serial.println("Starting BLE work!");
+    pinMode(LED,OUTPUT);
+    BLEDevice::init("Remote Mouse");
+    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9); 
+    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
+    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN ,ESP_PWR_LVL_P9);
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyCallbacks());
+    pServer->getPeerDevices(false);
 
-  hid = new BLEHIDDevice(pServer);
-  input = hid->inputReport(1); // <-- input REPORTID from report map
-  output = hid->outputReport(1); // <-- output REPORTID from report map
+    hid = new BLEHIDDevice(pServer);
+    input = hid->inputReport(1); // <-- input REPORTID from report map
+    output = hid->outputReport(1); // <-- output REPORTID from report map
 
-    std::string name = "Phone Lamp";    
-  hid->manufacturer()->setValue(name);
+    hid->manufacturer()->setValue("Remote Mouse");
 
-  hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
-  hid->hidInfo(0x00,0x02);
+    hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
+    hid->hidInfo(0x00,0x02);
 
-  BLESecurity *pSecurity = new BLESecurity();
-  //  pSecurity->setKeySize();
+    hid->inputReport(KEYBOARD_ID);
+    hid->outputReport(KEYBOARD_ID);
+    hid->inputReport(MEDIA_KEYS_ID);
+    hid->manufacturer("Dmytro M.");
+    BLESecurity *pSecurity = new BLESecurity();
     pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
 
     hid->startServices();
-
     pAdvertising = pServer->getAdvertising();
-    pAdvertising->setAppearance(HID_BARCODE);
+    pAdvertising->setAppearance(HID_MOUSE);
     pAdvertising->addServiceUUID(hid->hidService()->getUUID());
     pAdvertising->start();
     hid->setBatteryLevel(7);
@@ -149,11 +154,11 @@ void setup() {
 
 void loop() {
 
-  if(!connected){
-    pAdvertising->start();
-    connect_wait();
-  }  
-    digitalWrite(LED,ON);
+    if(!connected){
+      pAdvertising->start();
+      connect_wait();
+    }  
+      digitalWrite(LED,ON);
 
-  vTaskDelay(5/portTICK_PERIOD_MS);
+    vTaskDelay(5/portTICK_PERIOD_MS);
 }
