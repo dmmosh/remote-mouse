79baa34aaad3388b3739bae2b6a0e899a4cee4ee
dmmosh <evilsharkeater@gmail.com>
2025-01-15 21:36:09 -0500

    nothing of note

--- src/utils.cpp
+++ src/utils.cpp
@@ -10,17 +10,35 @@ void move(uint8_t x , uint8_t y, uint8_t xwheel, uint8_t ywheel){
 void my_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gatts_cb_param_t* param){
     Serial.println((int)event);
     switch (event) {
-        case ESP_GATTS_CONGEST_EVT:
-            if (param->congest.congested) {
-                Serial.println("CONGESTED");
-            } else {
-                Serial.println("CLEARED");
-            }
-            break;
+               case 0:       Serial.println("EVENT #0:   register application id");
+        break; case 1:       Serial.println("EVENT #1:   gatt client request read operation");
+        break; case 2:       Serial.println("EVENT #2:   gatt client request write operation");
+        break; case 3:       Serial.println("EVENT #3:   gatt client request execute write");
+        break; case 4:       Serial.println("EVENT #4:   set mtu complete");
+        break; case 5:       Serial.println("EVENT #5:   receive confirm");
+        break; case 6:       Serial.println("EVENT #6:   unregister application id");
+        break; case 7:       Serial.println("EVENT #7:   create service complete");
+        break; case 8:       Serial.println("EVENT #8:   add included service complete");
+        break; case 9:       Serial.println("EVENT #9:   add characteristic complete");
+        break; case 10:      Serial.println("EVENT #10:  add descriptor complete");
+        break; case 11:      Serial.println("EVENT #11:  delete service complete");
+        break; case 12:      Serial.println("EVENT #12:  start service complete");
+        break; case 13:      Serial.println("EVENT #13:  stop service complete");
+        break; case 14:      Serial.println("EVENT #14:  gatt client connect");
+        break; case 15:      Serial.println("EVENT #15:  gatt client disconnect");
+        break; case 16:      Serial.println("EVENT #16:  connect to peer");
+        break; case 17:      Serial.println("EVENT #17:  disconnect from peer");
+        break; case 18:      Serial.println("EVENT #18:  gatt server close");
+        break; case 19:      Serial.println("EVENT #19:  gatt listen to be connected the event comes");
+        break; case 20:      Serial.println("EVENT #20:  congest happen");
+        break; case 21:      Serial.println("EVENT #21:  gatt send response complete");
+        break; case 22:      Serial.println("EVENT #22:  gatt create table complete");
+        break; case 23:      Serial.println("EVENT #23:  gatt set attr value complete");
+        break; case 24:      Serial.println("EVENT #24:  gatt send service change indication complete");
 
         // Handle other BLE GATT server events as needed
         default:
-            break;
+            Serial.printf("EVENT #%i: unknown", event);
     }
 }
 
