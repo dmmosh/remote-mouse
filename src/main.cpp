7c93b3ffab4ee078ee5fcd865d7b81db62e62483
dmmosh <evilsharkeater@gmail.com>
2025-01-15 22:09:42 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -76,8 +76,9 @@ void setup() {
     //output = hid->outputReport(1); // <-- output REPORTID from report map
 
 
-     hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
-     hid->hidInfo(0x00,0x02);
+
+    hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
+    hid->hidInfo(0x00,0x02);
 
     BLESecurity *pSecurity = new BLESecurity();
     pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
@@ -91,10 +92,7 @@ void setup() {
     pAdvertising->addServiceUUID(hid->hidService()->getUUID());
     pAdvertising->start();
 
-    pBLEScan = BLEDevice::getScan();  
-    pBLEScan->setActiveScan(true);  // Set active scan to get more information (e.g., RSSI)
-    pBLEScan->setInterval(100);     // Set scan interval (in milliseconds)
-    pBLEScan->setWindow(99);        // Set scan window (in milliseconds)
+   
     
     hid->setBatteryLevel(100);
   
