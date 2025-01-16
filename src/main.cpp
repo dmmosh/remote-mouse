a13bb6f118a21bf6a27c045abc6cfe5ad20839f0
dmmosh <evilsharkeater@gmail.com>
2025-01-15 20:53:21 -0500

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -17,37 +17,7 @@ void setup() {
     Serial.begin(115200);
     Serial.println("Starting BLE work!");
     pinMode(LED,OUTPUT);
-    BLEDevice::init("Remote Mouse");
-    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9); 
-    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
-    esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN ,ESP_PWR_LVL_P9);
-    pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyCallbacks());
-    pServer->getPeerDevices(false);
-
-    hid = new BLEHIDDevice(pServer);
-    input = hid->inputReport(1); // <-- input REPORTID from report map
-    output = hid->outputReport(1); // <-- output REPORTID from report map
-
-    hid->manufacturer()->setValue("Dmytro M.");
-
-    hid->pnp(0x02, 0xe502, 0xa111, 0x0210);
-    hid->hidInfo(0x00,0x02);
-
-    BLESecurity *pSecurity = new BLESecurity();
-    pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
-
-    hid->startServices();
-    pAdvertising = pServer->getAdvertising();
-    pAdvertising->setAppearance(HID_MOUSE);
-    pAdvertising->addServiceUUID(hid->hidService()->getUUID());
-    pAdvertising->start();
-    hid->setBatteryLevel(100);
-
-    pBLEScan = BLEDevice::getScan();  
-    pBLEScan->setActiveScan(true);  // Set active scan to get more information (e.g., RSSI)
-    pBLEScan->setInterval(100);     // Set scan interval (in milliseconds)
-    pBLEScan->setWindow(99);        // Set scan window (in milliseconds)
+    
     digitalWrite(LED,ON);
     
     mouse->begin();
@@ -63,6 +33,6 @@ void loop() {
       pAdvertising->start();
       connect_wait();
     }  
-    mouse->move(1,1);
+    
     vTaskDelay(10/portTICK_PERIOD_MS);
 }
