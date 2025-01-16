1ab8142ae471516f92139480aa2ff8f981ceb438
dmmosh <evilsharkeater@gmail.com>
2025-01-15 21:37:07 -0500

    nothing of note

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,6 +7,39 @@ void move(uint8_t x , uint8_t y, uint8_t xwheel, uint8_t ywheel){
     input->notify();
 }
 
+ void connect_wait(){
+    uint16_t ms_5  = 0;
+    uint16_t seconds = 0;
+    pinMode(LED,OUTPUT);
+
+    while(!connected){
+        
+        if(ms_5 >= 200){    
+            if(seconds<21){ // note: at 20 second mark, it sets led to LOW and it stays there afterwards (to save power and not be annoying)
+                digitalWrite(LED,seconds%2);
+            }
+
+            Serial.printf("Waiting for device to pair... %is\n", seconds);
+            seconds++;
+            ms_5 = 0;
+        } else {
+        ms_5++;
+        }
+        vTaskDelay(5/portTICK_PERIOD_MS);
+    }
+}
+
+bool str_equals(const char* str1, const char* str2){
+    uint8_t j = 0;
+    while(str1[j] != '\0' || str2[j] != '\0'){
+        if (str1[j] != str2[j]){
+            return false;
+        }
+        j++;
+    }
+    return true;
+}
+
 void my_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gatts_cb_param_t* param){
     switch (event) {
                case 0:       Serial.println("EVENT #0:   register application id");
@@ -39,38 +72,4 @@ void my_gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gattc_if, 
         default:
             Serial.printf("EVENT #%i: unknown", event);
     }
-}
-
-
- void connect_wait(){
-    uint16_t ms_5  = 0;
-    uint16_t seconds = 0;
-    pinMode(LED,OUTPUT);
-
-    while(!connected){
-        
-        if(ms_5 >= 200){    
-            if(seconds<21){ // note: at 20 second mark, it sets led to LOW and it stays there afterwards (to save power and not be annoying)
-                digitalWrite(LED,seconds%2);
-            }
-
-            Serial.printf("Waiting for device to pair... %is\n", seconds);
-            seconds++;
-            ms_5 = 0;
-        } else {
-        ms_5++;
-        }
-        vTaskDelay(5/portTICK_PERIOD_MS);
-    }
-}
-
-bool str_equals(const char* str1, const char* str2){
-    uint8_t j = 0;
-    while(str1[j] != '\0' || str2[j] != '\0'){
-        if (str1[j] != str2[j]){
-            return false;
-        }
-        j++;
-    }
-    return true;
 }
