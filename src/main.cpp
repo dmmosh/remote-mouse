c2a8b8cfca424803d37ac3773bffcd06e5bec97d
dmmosh <evilsharkeater@gmail.com>
2025-07-18 15:59:07 -0400

    nothing of note

--- src/main.cpp
+++ src/main.cpp
@@ -126,14 +126,16 @@ void loop() {
     // }  
     
     
-    x = (21-analogRead(VRX)/128)>>2; // 2750 / 64 = 42
-    y = (21-analogRead(VRY)/128)>>2; // 2730 / 64 = 4
-    click = (bool)!digitalRead(SW);
+    // x = (21-analogRead(VRX)/128); // 2750 / 64 = 42
+    // y = (21-analogRead(VRY)/128); // 2730 / 64 = 4
+    // click = (bool)!digitalRead(SW);
 
-    x+= (x>0) ? -(x>>1) : 1;
-    y += (y>0) ? -(y>>1) : 1;
+    // x+= (x>0) ? -(x>>1) : 1;
+    // y += (y>0) ? -(y>>1) : 1;
 
+    x = analogRead(VRX);
 
+    y = analogRead(VRY);
 
 
 
