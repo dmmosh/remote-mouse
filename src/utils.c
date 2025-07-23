f4768f1664d97d9c850f1210e92b64986aa92d03
dmmosh <evilsharkeater@gmail.com>
2025-07-22 22:33:55 -0400

    nothing of note

--- src/utils.c
+++ src/utils.c
@@ -22,7 +22,7 @@ void mouse_move_task(void *pvParameters)
         int vrx, vry, sw;
         adc2_get_raw(VRX, ADC_WIDTH_BIT_12, &vrx);
         adc2_get_raw(VRY, ADC_WIDTH_BIT_12, &vry);
-        sw = (int)(!((bool)gpio_get_level(SW)));
+        sw = !gpio_get_level(SW);
         ESP_LOGI(TAG, "vrx: %i, vry: %i, sw: %i", vrx,vry,sw);
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
