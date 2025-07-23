c6bc2817b0d88b0201fdfce60c9d75d9cb198278
dmmosh <evilsharkeater@gmail.com>
2025-07-22 22:06:16 -0400

    nothing of note

--- src/utils.c
+++ src/utils.c
@@ -9,14 +9,14 @@ void mouse_move_task(void *pvParameters)
     ESP_LOGI(TAG, "starting");
     for (;;) {
         s_local_param.x_dir = 1;
-        int8_t step = 1;
+        int8_t step = 10;
         for (int i = 0; i < 2; i++) {
             xSemaphoreTake(s_local_param.mouse_mutex, portMAX_DELAY);
             s_local_param.x_dir *= -1;
             xSemaphoreGive(s_local_param.mouse_mutex);
             for (int j = 0; j < 500; j++) {
                 send_mouse_report(0, s_local_param.x_dir * step, 0, 0);
-                vTaskDelay(2 / portTICK_PERIOD_MS);
+                vTaskDelay(10 / portTICK_PERIOD_MS);
             }
         }
         vTaskDelay(1000 / portTICK_PERIOD_MS);
